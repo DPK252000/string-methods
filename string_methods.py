e83cf4039fe703396b88dcbe0e211b4b11ed1c95
DPK252000 <113545795+DPK252000@users.noreply.github.com>
2022-10-04 19:29:03 +0530

    Add files via upload

---
+++ string_methods.py
@@ -0,0 +1,104 @@
+txt = "python is FUN!"
+x = txt.capitalize()
+print (x)
+
+txt = "Hello, And Welcome To My World!"
+x = txt.casefold()
+print(x)
+
+txt = "banana"
+x = txt.center(20)
+print(x)
+
+txt = "I love apples, apple are my favorite fruit"
+x = txt.count("apple")
+print(x)
+
+txt = "My name is Ståle"
+x = txt.encode()
+print(x)
+
+txt = "Hello, welcome to my world."
+x = txt.endswith(".")
+print(x)
+
+txt = "H\te\tl\tl\to"
+x =  txt.expandtabs(2)
+print(x)
+
+txt = "Hello, welcome to my world."
+x = txt.find("welcome")
+print(x)
+
+txt = "For only {price:.2f} dollars!"
+print(txt.format(price = 49))
+
+txt = "Hello, welcome to my world."
+x = txt.index("welcome")
+print(x)
+
+txt = "Company12"
+x = txt.isalnum()
+print(x)
+
+txt = "CompanyX"
+x = txt.isalpha()
+print(x)
+
+txt = "Company123"
+x = txt.isascii()
+print(x)
+
+txt = "\u0033" #unicode for 3
+x = txt.isdecimal()
+print(x)
+
+txt = "50800"
+x = txt.isdigit()
+print(x)
+
+txt = "Demo"
+x = txt.isidentifier()
+print(x)
+
+txt = "hello world!"
+x = txt.islower()
+print(x)
+
+txt = "565543"
+x = txt.isnumeric()
+print(x)
+
+txt = "Hello! Are you #1?"
+x = txt.isprintable()
+print(x)
+
+txt = "   "
+x = txt.isspace()
+print(x)
+
+txt = "Hello, And Welcome To My World!"
+x = txt.istitle()
+print(x)
+
+txt = "THIS IS NOW!"
+x = txt.isupper()
+print(x)
+
+myTuple = ("John", "Peter", "Vicky")
+x = "#".join(myTuple)
+print(x)
+
+txt = "banana"
+x = txt.ljust(20)
+print(x, "is my favorite fruit.")
+
+txt = "Hello my FRIENDS"
+x = txt.lower()
+print(x)
+
+
+
+
+
+
